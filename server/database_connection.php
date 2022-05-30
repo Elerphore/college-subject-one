9658acbb306551661bf8dea012b335ab055cf7f1
Eremey Malinovsky <Elerphore@gmail.com>
2022-05-30 20:27:59 +0500

    *

--- server/database_connection.php
+++ server/database_connection.php
@@ -1,2 +1,8 @@
 <?php
+try {
+    $pdo = new PDO('mysql:host=127.0.0.1; port=3306; dbname=vethospital', 'root', '');
+    $pdo->exec('SET NAMES "utf8"');
+} catch(PDOException $e) {
+    echo($e);
+}
 ?>
